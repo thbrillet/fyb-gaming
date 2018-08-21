05572c77315245ed1751a945472d5586ef3025e9
Frederic Kaveh <heydarkv@yahoo.fr>
2018-08-21 15:00:35 +0200

    premieres seed ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,64 +8,75 @@
 puts "Cleaning database..."
 User.destroy_all
 Game.destroy_all
+Group.destroy_all
 Membership.destroy_all
+UserGame.destroy_all
+
 
 puts "Creating default users..."
 
 pierre = User.create!(
   username: "pagehey",
-  avatar: File.open(Rails.root.join("db/fixtures/users/pierre.jpeg")),
+  photo: File.open(Rails.root.join("db/fixtures/users/pierre.jpg")),
   biography: "J'aime les jeux d'acion et realiser de grosse performance en e-gaming",
   punch_line: "Go Go Go! on les degomme!",
   language: "fr",
   birthdate: "1989-10-06",
-  interest_1: "PVP",
-  interest_2: "FPS",
-  interest_3: "Story",
-  interest_4: "Compet",
-  interest_5: "SmallTeam"
+  interest_pve_pvp: "PVP",
+  interest_action_strat: "FPS",
+  interest_gameplay_story: "Story",
+  interest_compet_chill: "Compet",
+  interest_mmo_smallteam: "SmallTeam",
+  email: "pierre@gmail.com",
+  password: "azerty"
 )
 thibault = User.create!(
   username: "La gachette",
-  avatar: File.open(Rails.root.join("db/fixtures/users/thibault.jpeg")),
+  photo: File.open(Rails.root.join("db/fixtures/users/thibault.jpg")),
   biography: "Je suis un être etrange mais créatif. J'aime construire, collectionner/
   et montrer mes chefs d'oeuvre. Validez mes esquisses et je serai votre ami!",
   punch_line: "Quand c'est beau, c'est thibault.",
   language: "fr",
   birthdate: "1985-6-16",
-  interest_1: "PVE",
-  interest_2: "STR",
-  interest_3: "Gameplay",
-  interest_4: "Chill",
-  interest_5: "MMO"
+  interest_pve_pvp: "PVE",
+  interest_action_strat: "STR",
+  interest_gameplay_story: "Gameplay",
+  interest_compet_chill: "Chill",
+  interest_mmo_smallteam: "MMO",
+  email: "thibault@gmail.com",
+  password: "azerty"
 )
 fred = User.create!(
   username: "Frodo_dodo",
-  avatar: File.open(Rails.root.join("db/fixtures/users/fred.jpeg")),
+  photo: File.open(Rails.root.join("db/fixtures/users/fred.jpg")),
   biography:"J'ai jamais le temps de jouer, c'est pas juste, même en prenant sur /
   mes heures de sommeils. Allez montrez moi ce que vous valez!",
   punch_line: "J'ai hate de jouer!",
   language: "fr",
   birthdate: "1979-8-7",
-  interest_1: "PVE",
-  interest_2: "STR",
-  interest_3: "Story",
-  interest_4: "Chill",
-  interest_5: "SmallTeam"
+  interest_pve_pvp: "PVE",
+  interest_action_strat: "STR",
+  interest_gameplay_story: "Story",
+  interest_compet_chill: "Chill",
+  interest_mmo_smallteam: "SmallTeam",
+  email: "fred@gmail.com",
+  password: "azerty"
 )
 guillaume = User.create!(
   username: "Piccolo",
-  avatar: File.open(Rails.root.join("db/fixtures/users/guillaume.jpeg")),
+  photo: File.open(Rails.root.join("db/fixtures/users/guillaume.jpg")),
   biography:"Ceratins crois que je suis proffesseur mais que je suis un tueur in game/
   vous allez voir que c'est moi le meilleur",
   punch_line: "There can be only one!",
   language: "fr",
   birthdate: "1982-4-5",
-  interest_1: "PVP",
-  interest_2: "STR",
-  interest_3: "Gameplay",
-  interest_4: "Chill",
-  interest_5: "MMO"
+  interest_pve_pvp: "PVP",
+  interest_action_strat: "STR",
+  interest_gameplay_story: "Gameplay",
+  interest_compet_chill: "Chill",
+  interest_mmo_smallteam: "MMO",
+  email: "guillaume@gmail.com",
+  password: "azerty"
 )
 
 puts "Creating default game..."
@@ -82,7 +93,7 @@ game_pc_guildwars2 = Game.create!(
   platform: "PC",
   year: "2015",
   genre: "MMO",
-  photo: File.open(Rails.root.join("db/fixtures/games/guildwars2.jpg"))
+  photo: File.open(Rails.root.join("db/fixtures/games/gw2.jpg"))
 )
 game_tablet_summonerswars = Game.create!(
   name: "Summoners_Wars",
@@ -167,31 +178,31 @@ user_game_4 = UserGame.new(
   game: game_tablet_summonerswars
 )
 user_game_5 = UserGame.new(
-  user: pierre
+  user: pierre,
   game: game_xboxone_halowars2
 )
 user_game_6 = UserGame.new(
-  user: guillaume
+  user: guillaume,
   game: game_xboxone_halowars2
 )
 user_game_7 = UserGame.new(
-  user: thibault
+  user: thibault,
   game: game_xboxone_halowars2
 )
 user_game_8 = UserGame.new(
-  user: pierre
+  user: pierre,
   game: game_pc_diablo3
 )
 user_game_9 = UserGame.new(
-  user: guillaume
+  user: guillaume,
   game: game_pc_diablo3
 )
 user_game_10 = UserGame.new(
-  user: thibault
+  user: thibault,
   game: game_pc_diablo3
 )
-user_game_10 = UserGame.new(
-  user: fred
+user_game_11 = UserGame.new(
+  user: fred,
   game: game_pc_diablo3
 )
 
@@ -202,21 +213,21 @@ group_1 = Group.create!(
   leader: thibault,
   name: "Labyrinth-Exploration",
   description: "Let's kill Tartaros",
-  photo: File.open(Rails.root.join("db/fixtures/games/summonerswars.jpg")
+  photo: File.open(Rails.root.join("db/fixtures/games/sws.png"))
 )
 group_2 = Group.create!(
   game: game_xboxone_halowars2,
   leader: pierre,
   name: "Shoot them all",
   description: "Pas de cheat içi, on couvre son voisin, ok?",
-  photo: File.open(Rails.root.join("db/fixtures/games/halo.jpg")
+  photo: File.open(Rails.root.join("db/fixtures/games/halo.jpg"))
 )
 group_3 = Group.create!(
   game: game_pc_diablo3,
   leader: guillaume,
   name: "Sortie stuff high level",
   description: "on farm les derniers niveaux faites gaffes",
-  photo: File.open(Rails.root.join("db/fixtures/games/diablo3.jpg")
+  photo: File.open(Rails.root.join("db/fixtures/games/diablo3.jpg"))
 )
 
 puts "Creating membership...."
@@ -224,25 +235,25 @@ puts "Creating membership...."
 user_membership_1 = Membership.new(
   user: fred,
   group: group_1,
-  statut: "pendding",
+  status: "pendding",
   message: "salut les gars, je suis bon, pls accept!"
 )
 user_membership_2 = Membership.new(
   user: thibault,
   group: group_2,
-  statut: "pendding",
+  status: "pendding",
   message: "Je veux shooter moi aussi!"
 )
 user_membership_3 = Membership.new(
   user: fred,
   group: group_3,
-  statut: "pendding",
+  status: "pendding",
   message: "Je veux farmer moi aussi!"
 )
 user_membership_4 = Membership.new(
   user: pierre,
   group: group_3,
-  statut: "pendding",
+  status: "pendding",
   message: "moi aussi j'ai besoin d'un meilleur stuff!"
 )
 
