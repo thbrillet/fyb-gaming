dbf717403be1dd1d78d5bd3e997b3761da20cfcf
Thibault Brillet <thbrillet@gmail.com>
2018-08-23 23:33:54 +0200

    avancées sur la show du group

--- app/javascript/components/groupmembers.js
+++ app/javascript/components/groupmembers.js
@@ -15,5 +15,23 @@ function ClickOnButton() {
   });
 };
 
+function ClickOnButtonShow() {
+  const btnshow = document.querySelectorAll(".btngroupshow");
+  [].forEach.call(btnshow, (btn) => {
+    btn.addEventListener('click', (event) => {
+      console.log('yep')
+      let chartDiv = btn.parentElement.parentElement.parentElement.parentElement
+      if(chartDiv.style.bottom == '-20vh') {
+        chartDiv.style.bottom = '60vh';
+        btn.innerHTML = "<span></span>#HIDE_MEMBERS";}
+      else {
+        console.log('else')
+        chartDiv.style.bottom = '-20vh';
+        btn.innerHTML = "<span></span>#SEE_MEMBERS";}
+    });
+  });
+};
+
 export { ClickOnButton };
+export { ClickOnButtonShow };
 
