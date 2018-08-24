218076e31b9383237c2414f0945a433174646bc7
Thibault Brillet <thbrillet@gmail.com>
2018-08-24 15:38:31 +0200

    fix deploy

--- app/javascript/components/fadeout.js
+++ app/javascript/components/fadeout.js
@@ -1,4 +1,4 @@
-const s = document.getElementById('alert').style;
-s.opacity = 1;
-(function fade(){(s.opacity-=.01)<0?s.display="none":setTimeout(fade,50)})();
+// const s = document.getElementById('alert').style;
+// s.opacity = 1;
+// (function fade(){(s.opacity-=.01)<0?s.display="none":setTimeout(fade,50)})();
 
