65f26414fd2d0dd696695b3ffcfefb4c51492c71
Pierre Tissot <pierregabriel@me.com>
2018-08-27 16:46:25 +0200

    basic profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: :show
+  before_action :set_user, only: [:show, :update]
 
   def profile
     @user = current_user
@@ -9,13 +9,20 @@ class UsersController < ApplicationController
   end
 
   def update
-    raise
+    @user.update(user_params)
+    redirect_to profile_path
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:interest_pve_pvp, :interest_mmo_smallteam, :interest_gameplay_story, :interest_compet_chill, :interest_action_strat, :username)
+    params.require(:user).permit(
+      :interest_pve_pvp,
+      :interest_mmo_smallteam,
+      :interest_gameplay_story,
+      :interest_compet_chill,
+      :interest_action_strat
+      )
   end
 
   def set_user
